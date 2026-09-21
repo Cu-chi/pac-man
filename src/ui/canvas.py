e870f395971ffbdab107f638f8ca67c4d3de886a
Matteo Chauvin <mchauvin@z2r5p5.42lyon.fr>
2026-09-21 14:27:08 +0200

    feat(canvas): added draw_rect function

--- src/ui/canvas.py
+++ src/ui/canvas.py
@@ -23,6 +23,11 @@ class Canvas:
         pygame.display.flip()
 
     def tick(self, fps: int) -> float:
-        res: int = self._clock.tick(fps)
+        res = self._clock.tick(fps)
         res_ms = res / 1000
         return float(res_ms)
+
+    def draw_rect(self, x: int, y: int, w: int, h: int,
+                  color: Color, filled: bool = True) -> None:
+        filling = 0 if filled else 1
+        pygame.draw.rect(self._screen, color, (x, y, w, h), filling)
