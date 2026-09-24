f274a01fe97293a8dec576444c867934866be9ae
Cu-chi <110144918+Cu-chi@users.noreply.github.com>
2026-09-24 10:12:51 +0200

    refactor(models): add Data suffix to Player and Ghost dataclasses

--- src/models.py
+++ src/models.py
@@ -88,7 +88,7 @@ class Entity():
 
 
 @dataclass(slots=True, kw_only=True)
-class Player(Entity):
+class PlayerData(Entity):
 
     lives: int
     score: int
@@ -103,7 +103,7 @@ class GhostState(Enum):
 
 
 @dataclass(slots=True, kw_only=True)
-class Ghost(Entity):
+class GhostData(Entity):
 
     state: GhostState
     color: str
@@ -132,8 +132,8 @@ class GameEvent(Enum):
 @dataclass(slots=True, kw_only=True)
 class GameState():
 
-    player: Player
-    ghosts: list[Ghost]
+    player: PlayerData
+    ghosts: list[GhostData]
     level: Level
     level_index: int
     total_levels: int
